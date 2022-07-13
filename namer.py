7c98ca528f5c9a646b47cecc52aca3a8562ee24c
max <anapa.igor@yandex.ru>
2022-07-13 16:07:48 +0300

    -reformat code;

--- namer.py
+++ namer.py
@@ -7,8 +7,8 @@ def namer():
         (500, 1120), (1250, 1120), (1950, 1120), (2650, 1120),
         (500, 2190), (1250, 2190), (1950, 2190), (2650, 2190),
     )
-    font = ImageFont.truetype(font='/home/max/PycharmProjects/photo_table/static/arial.ttf', size=100)
-    path_image = f'/home/max/PycharmProjects/photo_table/static/completed/new_photo_1.jpg'
+    font = ImageFont.truetype(font='static/arial.ttf', size=100)
+    path_image = f'static/completed/new_photo_1.jpg'
     image_a4 = Image.open(path_image)
     back_img = image_a4.copy()
     draw_text = ImageDraw.Draw(back_img)
@@ -23,7 +23,7 @@ def namer():
             font=font
         )
         num_animator += 1
-    back_img.save(f'/home/max/PycharmProjects/photo_table/static/completed/named_photo_1.jpg', quality=100)
+    back_img.save(f'static/completed/named_photo_1.jpg', quality=100)
     back_img.show()
     print('Имена выведены.')
 
